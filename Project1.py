2a44b0726a665aeee1e7652ce7f22ca7ce4451a2
Benjamin-Webb <benjamin.webb@asu.edu>
2022-10-21 18:23:12 -0700

    Started project, more updates

--- Project1.py
+++ Project1.py
@@ -39,5 +39,68 @@ class Dynamics(nn.Module):
 		# Apply thrust
 		delta_thrust = BOOST_ACCEL * FRAME_TIME * torch.tensor([0.0, -1.0], dtype=torch.double) * action
 
-		# Update state vector
+		# Update velocity
 		state = state + delta_thrust + delta_gravity
+
+		# Update state vector
+		step_mat = torch.tensor([[1.0, FRAME_TIME],
+		                        [0.0, 1.0]], dtype=torch.double)
+		state = torch.matmul(step_mat, state)
+
+		return state
+
+# Define Controller Class
+class Controller(nn.Module):
+
+	# Initialize class
+	def __init__(self, dim_input, dim_hidden, dim_output):
+		# dim_input: # of system states
+		# dim_output: # of actions
+		# dim_hidden: TBD, currently set to 1
+
+		super(Controller, self).__init__()
+		self.network = nn.Sequential(nn.Linear(dim_input, dim_hidden),
+		                             nn.Tanh(), nn.Linear(dim_hidden, dim_output),
+		                             nn.Sigmoid())
+
+	# define Controller forward method
+	def forward(self, state):
+		action = self.network(state)
+		return action
+
+# Define Simulation Class
+class Simulation(nn.Module):
+
+	# Initialize Class
+	def __init__(self, controller, dynamics, T):
+		super(Simulation, self).__init__()
+		self.state = self.initialize_state()
+		self.controller = controller
+		self.dynamics = dynamics
+		self.T = T
+		self.action_trajectory = []
+		self.state_trajectory = []
+
+	# Define Simulation class forward method
+	def forward(self, state):
+		self.action_trajectory = []
+		self.state_trajectory = []
+		for _ in range(self.T):
+			action = self.controller.forward(state)
+			state = self.dynamics.forward(state, action)
+			self.action_trajectory.append(action)
+			self.state_trajectory.append(state)
+
+		return self.error(state)
+
+	@staticmethod
+	def intialize_state():
+		state = [1.0, 0.0]          # Need to update
+
+		return torch.tensor(data=state, dtype=torch.double, requires_grad=False)
+
+	# Define Simulation class error, will need to be updated for increased state variables
+	@staticmethod
+	def error(state):
+		return state[0]**2 + state[1]**2
+
